38a70e4977f178c16160b9b907cfe9b8a2578356
Juan Carlos <juandev33@gmail.com>
2022-06-25 02:24:46 -0300

    update2

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
-import { gql, useQuery } from "@apollo/client"
 import { Event } from "./pages/Event";
 
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
-
 export function App() {
- const { data } = useQuery(GET_LESSONS_QUERY)
-
   return (
     <Event />
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { Logo } from "../images/logo";
+import { Logo } from "../images/Logo";
 
 export function Header() {
     return (
-        <header className="flex justify-center items-center w-full py-5">
+        <header className="flex justify-center items-center w-full py-5 bg-gray-700 border-b border-gray-600">
             <Logo />
         </header>
     )
